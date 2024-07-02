f12bbac00e45f0ea3d069d9011e7824f202db0f2
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-02 05:56:05 +0000

    conversion of datatypes

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,9 +1,32 @@
-let score="33"
+// let score="33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof(score));
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" = NaN
+//true=> 1 ; false=> 0
+
+// let isLoggedIn= "tahir"
+
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1=> true
+// 0=> false
+// "tahir"=> true: ""=>false
+
+
+let someNumber= 33
+console.log(typeof someNumber);
+let stringnumber= String(someNumber)
+console.log(stringnumber);
+console.log(typeof(stringnumber));
 
