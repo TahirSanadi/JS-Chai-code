d5d31f5234dcb7de46ecd8edadde4dc487aef904
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-02 06:45:51 +0000

    memory

--- 01_basics/Datatypes-summry.js
+++ 01_basics/Datatypes-summry.js
@@ -1,31 +1,54 @@
 
-//#Datatypes
-// Primitive
-// :string,number,boolean,null,undefined,symbol,BigInt
+// //#Datatypes
+// // Primitive
+// // :string,number,boolean,null,undefined,symbol,BigInt
 
-const score =  100
-const scoreValue= 100.3
-const isLoggedIn = false 
-const ousideTemp = null
-let userEmail; 
-const id= Symbol('123')
-const anotherId=Symbol('123')
+// // const score =  100
+// // const scoreValue= 100.3
+// // const isLoggedIn = false 
+// // const ousideTemp = null
+// // let userEmail; 
+// // const id= Symbol('123')
+// // const anotherId=Symbol('123')
 
-console.log(id===anotherId);
+// // console.log(id===anotherId);
 
-const BigNumber = 1646534476528734413123n
+// // const BigNumber = 1646534476528734413123n
 
-// Reference/non-primitive:Array,Objects, Function
+// // // Reference/non-primitive:Array,Objects, Function
 
-const heros = ["Captain", "Hulk", "Thor"]
+// // const heros = ["Captain", "Hulk", "Thor"]
 
-let myobj={
-    name:"tahir",
-    age:23,
-}
+// // let myobj={
+// //     name:"tahir",
+// //     age:23,
+// // }
 
-const myfunction=  function (){
-    console.log("HEllO");
-}
-// ()
-console.log(typeof myfunction);
+// // const myfunction=  function (){
+// //     console.log("HEllO");
+// // }
+// // // ()
+// // console.log(typeof myfunction);
+
+// /*************************************************************************Memory************************ */
+
+// //Stack(Premitive)
+// //Heap(reference/Nonprimitive)
+
+// let myYoutubename="Welth making"
+
+// let anothername= "Tahir codes"
+// console.log(myYoutubename)
+// console.log(anothername);
+
+// let user={
+//     email:"tahir@gmail.com",
+//     upi:"user@ybl"
+
+// }
+// console.log(user.email);
+// let user2=user
+// user2.email="sanadi@gmail.com"
+
+// console.log(user.email);
+// console.log(user2.email);
