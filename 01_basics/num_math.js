ff3d117e3d940b29dd2c2b299624a9a34019a495
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-03 06:45:48 +0000

    math

---
+++ 01_basics/num_math.js
@@ -0,0 +1,23 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(4));
+// //op=>23.90
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/***************maths*********************/
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.min(3, 4, 5, 7, 2));
+console.log((Math.random()*10)+1);
+
