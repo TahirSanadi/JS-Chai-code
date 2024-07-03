0bc99964010881ac06b2b1c39dda37d12b44fec9
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-03 13:08:01 +0000

    array

---
+++ 01_basics/Array.js
@@ -0,0 +1,36 @@
+const myArr=[0,1,2,3,4,5]
+const hero=['captrain','hulk','thor']
+
+const myArr2 = new Array(6,7)
+
+//in javascript the arrays are desizable
+
+// console.log(myArr);
+// console.log(myArr2);
+// myArr.push(6)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(8)
+// console.log(myArr);
+// myArr.shift(5)
+// console.log(myArr);
+
+// const newArr =myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+//slice , splice
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B",myArr);
+const myn2 = myArr.splice(1,3);
+console.log("C",myArr);
+console.log(myn2);
+
+ 
