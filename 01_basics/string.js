9fd5ede1345ba5dcb77c5359342850f7a0ca591e
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-03 06:07:30 +0000

    string commit

---
+++ 01_basics/string.js
@@ -0,0 +1,19 @@
+const name = 'tahir'
+const age = 23
+
+// console.log(`hello my name is ${name} and i am ${age} years old`);
+
+const gamename= new String("tahirsanadi")
+
+//console.log(gamename[0]);
+
+//console.log(gamename.__proto__);
+
+//write this in any browsers console u will get all methods of string 
+
+// console.log(gamename.length);
+// console.log(gamename.indexOf('r'));
+// const a='       TAHIR        '
+// console.log(a);
+// console.log(a.trim());
+// console.log(a.toLowerCase().trim());
