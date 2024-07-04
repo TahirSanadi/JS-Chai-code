3ceaa762d009630b43a61f2f034b56a5751b3b1a
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-04 05:30:57 +0000

    array 2.0

--- 01_basics/Array.js
+++ 01_basics/Array.js
@@ -23,14 +23,39 @@ const myArr2 = new Array(6,7)
 // console.log(typeof newArr);
 
 
-//slice , splice
+//slice(operates on copy of array) , splice(operated on origenal set of array)
 
-console.log("A",myArr);
-const myn1 = myArr.slice(1,3)
-console.log(myn1);
-console.log("B",myArr);
-const myn2 = myArr.splice(1,3);
-console.log("C",myArr);
-console.log(myn2);
+// console.log("A",myArr);
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+// console.log("B",myArr);
+// const myn2 = myArr.splice(1,3);
+// console.log("C",myArr);
+// console.log(myn2);
 
- 
+/***************Array Operation 2.0************/
+
+const heros_MCU=["thor","spiderman","hulk"]
+const heros_DC=["Batman","flash","superman"]
+
+// heros_MCU.push(heros_DC)
+
+// console.log(heros_MCU)
+
+// const allheros=heros_MCU.concat(heros_DC)
+// console.log(allheros);
+
+//spreade opperator(...)
+
+// const all_new_heros = [...heros_MCU,...heros_DC]
+
+// console.log(all_new_heros);
+
+//to convert data in array
+
+// console.log(Array.isArray("tahir"));
+// console.log(Array.from("tahir"));
+// console.log(Array.from({name:"tahir"}))//interesting
+
+
+
