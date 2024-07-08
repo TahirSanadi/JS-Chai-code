e69a3541a0556fec81d52f839787986483111dbd
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-08 09:34:07 +0000

    scope

--- 01_basics/Scope.js
+++ 01_basics/Scope.js
@@ -1,11 +1,54 @@
+//var c= 300
+// let a= 300
+// if(true){
+//     let a =10 
+//     const b= 20
+//     var c= 30
+//     console.log("inner",a);
+// }
 
+
+// console.log(a);
+//console.log(b);
+// console.log(c);
+
+
+
+// function one(){
+//     const username="tahir"
+//     function two(){
+//         const website= "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
+
+// one()
 
 if(true){
-    let a =10 
-    const b= 20
-    var c= 30
-
+    const username= "tahir"
+    if(username==="tahir"){
+        const website="tahir"
+        console.log(username + website);
+    }
+    // console.log(website);
 }
-// console.log(a);
-console.log(b);
-console.log(c);
+
+// console.log(username);
+
+//****************interesting ***************/
+
+console.log(addone(5))
+
+function addone(num){
+    return num +1 
+}
+
+
+
+
+// console.log(addTwo(5));
+const addTwo=function (num){
+    return num+2
+} 
