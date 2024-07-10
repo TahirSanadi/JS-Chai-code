e0434dfeca037f6ca07f8184b95d3b80c9449f99
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-10 06:07:22 +0000

    flow statement

---
+++ 01_basics/controlflow.js
@@ -0,0 +1,83 @@
+
+// const isuserLoggedin=true
+// const temp = 41
+// if(temp<50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("hi...");
+// }
+
+// const balance = 1000
+// if(balance<500){
+//     console.log("less that 500");
+// }else if(balance<750){
+//     console.log("less than 750");
+// }else if(balance<1200){
+//     console.log("less that 1200");
+// }
+// if(balance>500) console.log("test")//not to do
+
+// const userLoggedIn=true
+// const debitCard=true
+// const loggedInfromEmail=true
+// const loggedInfromGoogle=false
+
+// if(userLoggedIn && debitCard && 2==3){
+//     console.log("allow to use debit card");
+// }
+// if(loggedInfromEmail || loggedInfromGoogle){
+//     console.log("loggedIN");
+// }
+
+
+//syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month =3
+// switch (month ) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 2:
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("match defualt");
+//         break;
+// }
+
+const month ="jan"
+switch (month ) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("match defualt");
+        break;
+}
+
