7f590b1eab47e90f7de274bd94f9d3efaa37700d
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-18 12:48:43 +0000

    maps , forof , forin

--- 01_basics/Arrayloops.js
+++ 01_basics/Arrayloops.js
@@ -11,3 +11,47 @@ const greeting = "hello...!"
 for (const greet of greeting) {
     //console.log(`Each char is ${greet}`);
 }
+
+
+//Maps
+//cotains unique values only means doesnt have any duplicate value , also stores the data in order as per given
+
+const map = new Map()
+
+map.set('IN',"India")
+map.set('USA',"Unted States America")
+map.set('SP',"Spain")
+map.set('IN',"India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key,':-',value);
+}
+
+// const myObj = {
+//     'game1':'NFS',
+//     'game2':'GTA'
+// }
+
+// for (const [key , value] of myObj) {
+//     console.log(key,':-',value);
+// }
+//#gives error not works for objects
+
+
+//here is way using forin loop
+const myObject = {
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby'
+}
+for (const key in myObject) {
+    // console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+const programming = ["js", "rb", "py"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
