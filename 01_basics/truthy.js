b5a4e6558a674ab00fd563cb9c06ad87fab17b43
TahirSanadi <152688154+TahirSanadi@users.noreply.github.com>
2024-07-18 08:41:28 +0000

    NullishOperator

--- 01_basics/truthy.js
+++ 01_basics/truthy.js
@@ -12,3 +12,19 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length===0){
     console.log("object is empty");
 }
+
+//nullish coalescing operator(??):nullundefined
+
+// let val1;
+// val1=5??10
+// val1=null??10
+// val1=undefined??15
+// val1 =null??10??20
+// console.log(val1);
+
+
+//Terniary operator
+//condition ? true : false
+
+const icetea=100
+icetea <=80 ? console.log("less than 80") : console.log("more than 80");
